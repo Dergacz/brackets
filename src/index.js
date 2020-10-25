8cae2ee1380c1a54c6fb42a442800ce3a331e663
Stanislav Dergach <sats96dergach@gmail.com>
2020-10-25 13:25:14 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
-module.exports = function check(str, bracketsConfig) {
+module.exports = function check(str = [], bracketsConfig) {
+  const stack = [];
+  const open = ['(', '[', '{'];
+  const close = [')', ']', '}'];
+console.log(str);
+console.log(bracketsConfig);
+  for (let i = 0 ; i < str.length; i++){
+    if (open.includes(str[i])){
+      stack.push(str[i]);
+    }
+    else{
+      if (close.indexOf(str[i]) === open.indexOf(stack[stack.length - 1])){
+        stack.pop();
+      }
+     
+    }
+  }
+  console.log(stack.length === 0);
+  return stack.length === 0;
+
   // your solution
 }
